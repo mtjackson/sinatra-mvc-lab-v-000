d887269970db1158aff3ea19057560dbfc441b3b
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-17 12:04:10 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,37 +1,25 @@
 class PigLatinizer
-+
-+    attr_accessor :word
-+
-+    def initialize
-+    end
-+
-+    def piglatinize(sentence)
-+
-+        words = sentence.split(" ")
-+        output = []
-+
-+        words.each do |word|
-+            count = 0
-+            word.split("").each do |i|
-+              if i =~ /[^AEIOUaeiou]/
-+                  count += 1
-+              elsif i =~ /[AEIOUaeiou]/
-+                  if word[0] =~ /[AEIOUaeiou]/
-+                    output << "#{word}way"
-+                  elsif word[0..count] =~ /[^AEIOUaeiou]{#{count}}/
-+                    output << "#{word[count..-1]}#{word[0..count-1]}ay"
-+                  else
-+                    output << "#{word[1..-1]}#{word[0]}ay"
-+                  end
-+                  count = 0
-+
-+                  break
-+              end
-+            end
-+        end
-+
-+      return output.join(" ")
-+
-+    end
-+
-+end 
+
+  attr_accessor :word
+
+  def initialize
+  end
+
+  def piglatinize(text)
+    text_split = text.split(' ')
+    result = ""
+    text_split.each do |word| 
+      if "aeiouAEIOU".include?(word[0])
+        result += "#{word}way "
+      else
+        i=0
+        while !"aeiouAEIOU".include?(word[i])
+          i+=1
+        end
+        result += "#{word[i..-1]+word[0..i-1]}ay "
+      end
+    end
+    result.strip
+  end
+
+end 
